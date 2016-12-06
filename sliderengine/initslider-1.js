60b7922e682d0997a65769b4db18acb67796d66a
scoundrelsavior <scoundrelsavior@gmail.com>
2016-12-06 20:56:46 +0800

    Update initslider-1.js

--- sliderengine/initslider-1.js
+++ sliderengine/initslider-1.js
@@ -60,7 +60,7 @@ jQuery(document).ready(function(){
 
         showwatermark:false,
 
-        slideinterval:10000,
+        slideinterval:6000,
 
         watermarkpositioncss:"display:block;position:absolute;bottom:4px;right:4px;",
 
